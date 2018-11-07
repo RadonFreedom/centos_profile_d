6471d076b8614a97a61b0012648be1edab54cc46
root <root@localhost.localdomain>
2018-11-07 08:26:44 +0800

    first commit

---
+++ radon.sh
@@ -0,0 +1,19 @@
+# Radon's alias file
+
+# thefuck alias
+eval $(thefuck --alias f)
+
+# poweroff alias
+alias pwr='poweroff'
+
+# ps --forest alias
+alias psf='ps --forest'
+
+#ls -lF alias
+alias lf='ll -Fi '
+
+#ls -alF alias
+alias lfa='ll -aFi'
+
+#df -T alias
+alias dft='df -T'
